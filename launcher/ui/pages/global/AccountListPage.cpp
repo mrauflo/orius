c6cf775d4db4851e943a887eb6f4b97ba4c20f5e
Auflo <mrauflo@proton.me>
2024-12-05 18:58:27 +0000

    Update: branch name

--- launcher/ui/pages/global/AccountListPage.cpp
+++ launcher/ui/pages/global/AccountListPage.cpp
@@ -137,7 +137,7 @@ void AccountListPage::on_actionAddAuthlibInjector_triggered()
     MinecraftAccountPtr account = AuthlibInjectorLoginDialog::newAccount(
         this, tr("Please enter your username (sometimes an email address), password, and the URL of your API server."
                  "<br>"
-                 "See <a href=\"https://github.com/mrauflo/orius/blob/develop/doc/alternative-auth-servers.md\">this page</a> in "
+                 "See <a href=\"https://github.com/mrauflo/orius/blob/unstable/doc/alternative-auth-servers.md\">this page</a> in "
                  "the Orius Launcher's repository for a list of common API servers."
                  "<br><br>"
                  "<b>Caution!</b> Your username and password will be sent to the authentication server you specify!"));
